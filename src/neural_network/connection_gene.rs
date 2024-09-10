2e1c080c5129956e88b445383b88d602b9daa4cf
Arturr-H <artur.h.0427@gmail.com>
2024-09-10 18:15:37 +0200

    Add getters for `ConnectionGene`

--- src/neural_network/connection_gene.rs
+++ src/neural_network/connection_gene.rs
@@ -2,8 +2,8 @@
 /// A connection between two `NodeGenes`
 pub struct ConnectionGene {
     /// The index / ID of some node
-    node1: usize,
-    node2: usize,
+    node_in: usize,
+    node_out: usize,
 
     /// The strength of the bond between the 
     /// two nodes.
@@ -15,13 +15,19 @@ pub struct ConnectionGene {
 }
 
 impl ConnectionGene {
-    pub fn new(node1: usize, node2: usize, weight: f32) -> Self {
-        Self { node1, node2, weight, enabled: true }
+    pub fn new(node_in: usize, node_out: usize, weight: f32) -> Self {
+        Self { node_in, node_out, weight, enabled: true }
     }
+    
+    // Getters
+    pub fn node_in(&self) -> usize { self.node_in }
+    pub fn node_out(&self) -> usize { self.node_out }
+    pub fn weight(&self) -> f32 { self.weight }
+    pub fn enabled(&self) -> bool { self.enabled }
 }
 
 impl std::fmt::Debug for ConnectionGene {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Conn({}-{} {{{}}})", self.node1, self.node2, self.enabled)
+        write!(f, "{}-{}", self.node_in, self.node_out)
     }
 }
