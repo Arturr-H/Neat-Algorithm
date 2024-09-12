00fe96a19191dbd1f534e0af8dbf641183ae1888
Arturr-H <artur.h.0427@gmail.com>
2024-09-12 19:11:23 +0200

    Implement huge changes. Network now stores a reference to global occupied connections and owns local occupied connections, and fixed `create_connection` method to inherit innovation number for a connection gene if it already exists globally

--- src/neural_network/network.rs
+++ src/neural_network/network.rs
@@ -1,4 +1,4 @@
-use std::{collections::{HashMap, HashSet, VecDeque}, fmt::Debug, hash::Hash, process::Output};
+use std::{collections::{HashMap, HashSet, VecDeque}, fmt::Debug, hash::Hash, process::Output, sync::{Arc, Mutex}};
 use rand::{thread_rng, Rng};
 use super::{connection_gene::ConnectionGene, node_gene::{NodeGene, NodeGeneType}};
 
@@ -25,12 +25,27 @@ pub struct NeatNetwork {
     /// an unique index / ID
     node_gene_index: usize,
 
-    /// To check if we've already got a connection
-    /// between two nodes. NEEDS to be (min, max),
-    /// and by that I mean the first integer index
-    /// needs to be less than the second.
-    occupied_connections: HashSet<(usize, usize)>,
+    /// Reference to global innovation number
+    global_innovation: Arc<Mutex<usize>>,
 
+    /// An arc pointer pointing to the occupied connections
+    /// which are stored in the `Evolution` struct. 
+    global_occupied_connections: Arc<Mutex<HashMap<(usize, usize), usize>>>,
+
+    /// Only for checking if we already have a connection.
+    /// If we don't have a local connection, but the connection 
+    /// was found in occupied GLOBAL connections, then we will
+    /// insert a new connection with the innovation number of the
+    /// global connection
+    local_occupied_connections: HashSet<(usize, usize)>,
+}
+
+impl NeatNetwork {
+    /// Create a new NEAT network with `input` amount
+    /// of input neurons and `output` amount of output
+    /// neurons.
+    /// 
+    /// ## Global innovation
     /// Applied to new genes, which is used later
     /// for crossover mutation
     /// 
@@ -44,41 +59,54 @@ pub struct NeatNetwork {
     /// we'll only crossover genes with the same innovation
     /// number as they represent the same "idea", so we don't
     /// ruin the topology.
-    global_innovation: usize
-}
-
-impl NeatNetwork {
-    /// Create a new NEAT network with `input` amount
-    /// of input neurons and `output` amount of output
-    /// neurons.
-    pub fn new(input: usize, output: usize) -> Self {
+    pub fn new(
+        input: usize,
+        output: usize,
+        global_innovation: Arc<Mutex<usize>>,
+        global_occupied_connections: Arc<Mutex<HashMap<(usize, usize), usize>>>
+    ) -> Self {
         // Create node genes
         let mut node_genes = Vec::with_capacity(input + output);
         for i in 0..input {
-            node_genes.push(NodeGene::new(i, NodeGeneType::Input))
+            node_genes.push(NodeGene::new(i, NodeGeneType::Input));
         }
         for i in input..(input+output) {
-            node_genes.push(NodeGene::new(i, NodeGeneType::Ouptut))
+            node_genes.push(NodeGene::new(i, NodeGeneType::Ouptut));
         }
 
         // Create connections genes
+        let mut local_occupied_connections = HashSet::new();
         let mut connection_genes = Vec::new();
-        let mut occupied_connections = HashSet::new();
+        let mut local_innovation = 0;
         let mut rng = thread_rng();
 
-        // TODO 
-        for i in 0..rng.gen_range(0..(input + output)).max(1) {
-            let node_in = rng.gen_range(0..input); // input
-            let node_out = rng.gen_range(input..(input+output)); // output
+        // Create a connection between every single input and output node
+        for input_idx in 0..input {
+            for output_idx in input..(input + output) {
+                let (connection, should_increment) = Self::create_connection(
+                    input_idx, output_idx,
+                    rng.gen_range(0.05..0.2),
+                    global_occupied_connections.clone(),
+                    &mut local_occupied_connections,
+                    local_innovation
+                );
 
-            let connection = Self::create_connection(node_in, node_out, rng.gen_range(0.05..0.2), &mut occupied_connections);
-            if let Some(conn) = connection {
-                connection_genes.push(conn);
+                if should_increment { local_innovation += 1 };
+                if let Some(conn) = connection { connection_genes.push(conn); };
 
+                // We know for sure that these connections exist
+                // and won't be duplicates because of our for-loops
+                // local_occupied_connections.insert((input_idx, output_idx));
+                
                 // Register that we've created a new outgoing weight for the new node
-                node_genes[node_out].register_new_incoming(connection_genes.len() - 1);
+                node_genes[output_idx].register_new_incoming(connection_genes.len() - 1);
             }
         }
+
+        // Set the global innovation because the "starter"
+        // connection genes. We do -1 because we didn't set
+        // a single connection for the last incremented inno.
+        *global_innovation.lock().unwrap() = local_innovation - 1;
 
         Self {
             input_size: input,
@@ -87,29 +115,22 @@ impl NeatNetwork {
             node_genes,
             connection_genes,
             node_gene_index: input + output,
-            occupied_connections,
-            global_innovation: 0
+            global_innovation,
+            global_occupied_connections,
+            local_occupied_connections
         }
-
-    }
-
-    pub fn debug_add_node(&mut self) -> () {
-        self.node_genes.push(NodeGene::new(self.node_gene_index, NodeGeneType::Regular));
-        self.node_gene_index += 1;
-    }
-    pub fn debug_add_weight(&mut self, node_in: usize, node_out: usize) -> () {
-        self.connection_genes.push(ConnectionGene::new(node_in, node_out, 0.5));
-        self.node_genes[node_out].register_new_incoming(self.connection_genes.len() - 1);
     }
 
     /// Will randomly add a new gene
     pub fn mutate(&mut self) -> () {
         let mut rng = thread_rng();
         let will_be_node_gene = thread_rng().gen_bool(0.5);
-        
+        let current_innovation = self.get_innovation();
+
         // Split weight in half and place node in middle
         if will_be_node_gene {
             let length = self.connection_genes.len();
+            
             let gene = &mut self.connection_genes[rng.gen_range(0..length)];
             let gene_node_in = gene.node_in();
             let gene_node_out = gene.node_out();
@@ -120,22 +141,37 @@ impl NeatNetwork {
                 NodeGeneType::Regular,
             ));
 
-            let input_connection = Self::create_connection(gene_node_in, self.node_gene_index, 1.0, &mut self.occupied_connections);
-            let output_connection = Self::create_connection(self.node_gene_index, gene_node_out, gene.weight(), &mut self.occupied_connections);
+            let (input_connection, should_increment_ingoing) = Self::create_connection(
+                gene_node_in, self.node_gene_index,
+                1.0,
+                self.global_occupied_connections.clone(),
+                &mut self.local_occupied_connections,
+                current_innovation + 1
+            );
+            let (output_connection, should_increment_outgoing) = Self::create_connection(
+                self.node_gene_index, gene_node_out,
+                gene.weight(),
+                self.global_occupied_connections.clone(),
+                &mut self.local_occupied_connections,
+                current_innovation + 2
+            );
 
-            match (input_connection, output_connection) {
-                (Some(input), Some(output)) => {
-                    self.connection_genes.push(input);
-                    self.connection_genes.push(output);
+            // If the genes were actually created we increment the 
+            // innovation number accordingly to match the previous
+            // current_innovation + 1 and + 2
+            if should_increment_ingoing { self.increment_innovation(); };
+            if should_increment_outgoing { self.increment_innovation(); };
 
-                    // Register that we've created a new incoming weight
-                    // for the new node, and the updated node
-                    self.node_genes[self.node_gene_index].register_new_incoming(self.connection_genes.len() - 2);
-                    self.node_genes[gene_node_out].register_new_incoming(self.connection_genes.len() - 1);
-                },
-
-                _ => {}
-            }
+            // Register that we've created a new incoming weight
+            // for the new node, and the updated node and push connection
+            if let Some(input) = input_connection {
+                self.connection_genes.push(input);
+                self.node_genes[self.node_gene_index].register_new_incoming(self.connection_genes.len() - 2);
+            };
+            if let Some(output) = output_connection {
+                self.connection_genes.push(output);
+                self.node_genes[gene_node_out].register_new_incoming(self.connection_genes.len() - 1);
+            };
 
             self.node_gene_index += 1;
         }
@@ -148,10 +184,17 @@ impl NeatNetwork {
             let node_in = rng.gen_range(0..self.input_size); // input
             let node_out = rng.gen_range(self.input_size..(self.input_size+self.output_size)); // output
 
-            let connection = Self::create_connection(node_in, node_out, rng.gen_range(0.05..0.2), &mut self.occupied_connections);
-            if let Some(conn) = connection {
-                self.connection_genes.push(conn);
-            }
+            let (connection, should_increment) = Self::create_connection(
+                node_in, node_out,
+                rng.gen_range(0.05..0.2),
+                self.global_occupied_connections.clone(),
+                &mut self.local_occupied_connections,
+                current_innovation + 1
+            );
+
+            // Increase innovation to match the previous self.get_innovation() + 1
+            if should_increment { self.increment_innovation(); };
+            if let Some(conn) = connection { self.connection_genes.push(conn); };
         }
     }
 
@@ -176,33 +219,49 @@ impl NeatNetwork {
         todo!()
     }
 
-    /// Tries to create a new connection
+    /// Tries to create a new connection. If the connection already
+    /// exists, we return the connection but it has the innovation
+    /// number of the already existing connection gene, so we don't
+    /// create same connection genes with diffrent innovation numbers.
+    /// 
+    /// returns (Option<connection>, a) where a is a boolean indicating wether
+    /// we have created a new innovation number. If a is false this
+    /// function has instead taken the innovation number from a previous
+    /// connection or just not created a connection (we don't need to increase innovation)
+    /// 
+    /// returns (None, a) if we already have the local connection established
     fn create_connection(
         node_in: usize,
         node_out: usize,
         weight: f32,
-        occupied_connections: &mut HashSet<(usize, usize)>
-    ) -> Option<ConnectionGene> {
-        let mut connection_found = false;
-        let mut connection_attempts = 0;
-
-        while !connection_found {
-            if !occupied_connections.contains(&(node_in, node_out)) {
-                connection_found = true;
-            };
-
-            if connection_attempts > 10 {
-                break;
+        global_occupied_connections: Arc<Mutex<HashMap<(usize, usize), usize>>>,
+        local_occupied_connections: &mut HashSet<(usize, usize)>,
+        innovation_number: usize
+    ) -> (Option<ConnectionGene>, bool) {
+        let global_occupied_connections = &mut global_occupied_connections.lock().unwrap();
+        match global_occupied_connections.get(&(node_in, node_out)) {
+            Some(inherited_innovation) => {
+                // Now that we've found that this connection exists for other
+                // networks - we need to check if this connection also exists
+                // for out current network (local). That's why we now will
+                // check if this connection exists in local_occupied_connections
+                if local_occupied_connections.contains(&(node_in, node_out)) {
+                    // We return None because there's no need to
+                    // push any connections because they already exist
+                    (None, false)
+                }else {
+                    local_occupied_connections.insert((node_in, node_out));
+                    (Some(ConnectionGene::new(node_in, node_out, weight, *inherited_innovation)), false)
+                }
+            },
+            None => {
+                // If it doesn't exist in global connections, it won't exist
+                // in our local occupied connections.
+                // Returns with true because we've incremented
+                global_occupied_connections.insert((node_in, node_out), innovation_number);
+                local_occupied_connections.insert((node_in, node_out));
+                (Some(ConnectionGene::new(node_in, node_out, weight, innovation_number)), true)
             }
-
-            connection_attempts += 1;
-        }
-
-        if connection_found {
-            occupied_connections.insert((node_in, node_out));
-            Some(ConnectionGene::new(node_in, node_out, weight))
-        }else {
-            None
         }
     }
 
@@ -242,27 +301,6 @@ impl NeatNetwork {
             .iter().map(|e| e.activation()).collect()
     }
 
-    pub fn create_python_debug_plotting(&self) {
-        let nodes = &self.node_genes;
-        let mut node_string = String::new();
-        for (index, node) in nodes.iter().enumerate() {
-            let node_type = match node.node_type() {
-                NodeGeneType::Input => "Input",
-                NodeGeneType::Ouptut => "Output",
-                NodeGeneType::Regular => "Regular",
-            };
-            node_string.push_str(&format!("{}: {:?},", index, node_type));
-        }
-
-        let mut connection_string = String::new();
-
-        for connection in &self.connection_genes {
-            connection_string.push_str(&format!("({}, {}),", connection.node_in(), connection.node_out()));
-        }
-
-        println!("{}\n{}", connection_string, node_string)
-    }
-
     /// The degree of a node is the amount of weights which are connected to it. And the
     /// indegree is the number of weights coming in, and outdegree are the number going out.
     pub fn topological_sort(&self) -> Option<Vec<usize>> {
@@ -297,6 +335,16 @@ impl NeatNetwork {
             // Graph has a cycle, no valid topological sort
             None
         }
+    }
+
+    /// Increment global innovation number
+    pub fn increment_innovation(&self) -> usize {
+        let inno = &mut *self.global_innovation.lock().unwrap();
+        *inno += 1;
+        *inno
+    }
+    pub fn get_innovation(&self) -> usize {
+        *self.global_innovation.lock().unwrap()
     }
 }
 
