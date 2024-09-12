dd8cc4fe27e2111c38171bdc990a632072929d97
Arturr-H <artur.h.0427@gmail.com>
2024-09-12 16:00:22 +0200

    Test case for `Evolution` struct

--- src/main.rs
+++ src/main.rs
@@ -2,52 +2,48 @@
 
 /* Modules */
 mod neural_network;
+mod trainer;
 mod utils;
 
-use std::time::Duration;
-
 /* Imports */
+use std::time::Duration;
 use neural_network::network::NeatNetwork;
 use block_blast::board::{self, board::Board, board_error::PlacementError, cell::Cell};
+use trainer::evolution::{Evolution, EvolutionBuilder};
 
 fn main() -> () {
-    let mut net = NeatNetwork::new(
-        2,
-        1
-    );
-    println!("{net:#?}");
+    let mut evolution = Evolution::new()
+        .batch_size(100)
+        .with_input_nodes(64 /* All cells */ + 36 /* Tiles to choose from */)
+        .with_output_nodes(8/*x*/ + 8/*y - Coordinate for tile placement */ + 3 /* What tile buffer to choose */)
+        .set_fitness_function(score_network)
+        .build()
+        .generation();
+
 
     let xor = vec![((0.0, 0.0), 0.0),
                     ((1.0, 0.0), 1.0),
                     ((0.0, 1.0), 1.0),
                     ((1.0, 1.0), 0.0)];
-    
 
-    
-    net.mutate();
-    println!("{net:#?}");
+    // let mut all_values = Vec::new();
+    // for net in networks.iter_mut() {
+    //     let mut total = 0.0;
+    //     for input in xor.iter() {
+    //         let err = 1. - net.calculate_output(vec![input.0.0, input.0.1])[0];
+    //         total += err;
+    //     }
 
+    //     all_values.push(total / 4.);
+    // }
 
+    // let top_5 = top_n_with_indices(&all_values, 5);
+}
 
+fn score_network(network: &mut NeatNetwork) -> f32 {
+    let mut game = Board::new();
+    let mut score = 0.0;
 
-
-
-
-
-
-    // let mut net = NeatNetwork::new(
-    //     64 /* All cells */ + 36 /* Tiles to choose from */,
-    //     8/*x*/ + 8/*y - Coordinate for tile placement */ + 3 /* What tile buffer to choose */
-    // );
-    /*let restart = |game: &mut Board, score: usize| {
-        if score > 50 {
-            panic!("SHIT YOU ARe GOOD! GOOD JOB AI");
-        }else {
-            println!("{}RESTART \r\n", "\r\n".repeat(10));
-            game.restart();
-        }
-    };
-    
     loop {
         let mut cells: Vec<f32> = game.cells().iter().map(Cell::to_f32).collect();
         let mut tiles = vec![0.0; 36];
@@ -60,7 +56,7 @@ fn main() -> () {
         cells.append(&mut tiles);
 
         let input = cells;
-        let decision = net.feed_forward(input);
+        let decision = network.calculate_output(input);
 
         let x = utils::get_max_index_list(&decision[0..8]);
         let y = utils::get_max_index_list(&decision[8..16]);
@@ -74,29 +70,27 @@ fn main() -> () {
 
             match game.place() {
                 Ok(e) => {
-                    score = e;
+                    score = e as f32;
                     success = true;
                 },
                 Err(error) => {
                     match error {
                         PlacementError::OutOfBounds | PlacementError::InvalidPlacement => {
                             if attempt >= 7 {
-                                restart(&mut game, score);
-                                break;
+                                return score
                             }
                             attempt += 1;
                             continue;
                         },
                         PlacementError::GameOver => {
-                            restart(&mut game, score);
+                            return score
                         }
                     }
                 }
             };
         }
-        game.log();
-        // std::thread::sleep(Duration::from_secs(1));
-    }*/
-}
 
+        // game.log();
+    }
+}
 
