eb3aa250bf3e74f814a5dcbbf0e176e2f114edfd
Arturr-H <artur.h.0427@gmail.com>
2024-09-14 18:02:25 +0200

    Fix spelling error

--- src/trainer/evolution.rs
+++ src/trainer/evolution.rs
@@ -117,11 +117,11 @@ impl EvolutionBuilder {
         let activations = NetworkActivations::new(hidden_activation, output_activation);
         
         // Create species
-        let mut species: Vec<Species> = Vec::with_capacity(AMOUNT_OF_INITIAL_SPECIES);
+        let mut species: Vec<Species> = Vec::with_capacity(batch_size);
         let mut global_occupied_connections = Arc::new(Mutex::new(HashMap::new()));
         let global_innovation_number = Arc::new(Mutex::new(0));
 
-        for i in 0..AMOUNT_OF_INITIAL_SPECIES {
+        for i in 0..batch_size {
             let representative = NeatNetwork::new(
                 input_nodes,
                 output_nodes,
@@ -168,7 +168,7 @@ impl Evolution {
 
         for species in self.species.iter_mut() {
             species.compute_generation(self.fitness_function);
-            let score = species.previous_aveage_score();
+            let score = species.previous_average_score();
             total_average_score += score;
             if score > best_score {
                 best_score = score;
@@ -244,7 +244,6 @@ impl Evolution {
             child_genes,
         )
     }
-
 
     /// Distance
     pub fn distance(net1: &NeatNetwork, net2: &NeatNetwork) -> f32 {
