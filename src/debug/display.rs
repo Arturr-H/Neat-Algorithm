84c2f4ef0456f7eb505b74c376aa0c7d4dc91bc1
Arturr-H <artur.h.0427@gmail.com>
2024-09-17 16:33:27 +0200

    A lot of changes in display

--- src/debug/display.rs
+++ src/debug/display.rs
@@ -1,7 +1,7 @@
 /* Imports */
 use core::f32;
 use std::collections::HashMap;
-use eframe::{egui::{self, pos2, Align2, Color32, FontId, Key, Painter, Rect}, epaint::PathStroke};
+use eframe::{egui::{self, pos2, Align2, Color32, FontId, Key, Painter, Pos2, Rect}, epaint::PathStroke};
 use crate::{neural_network::{connection_gene::ConnectionGene, network::NeatNetwork, node_gene::{NodeGene, NodeGeneType}}, trainer::evolution::Evolution};
 
 /* Constants */
@@ -11,6 +11,14 @@ struct DrawContext {
     evolution: Evolution,
     species_index: usize,
     tab_height: f32,
+
+    /// Some(net_idx) if we've double-clicked on a
+    /// network and want to enlarge its picture.
+    focusing: Option<usize>,
+    info: Option<NodeGene>,
+
+    /// What we should save a network as
+    save_name: Option<String>,
 }
 pub fn start_debug_display(evolution: Evolution) -> () {
     let options = eframe::NativeOptions::default();
@@ -20,7 +28,10 @@ pub fn start_debug_display(evolution: Evolution) -> () {
         Box::new(|_cc| Ok(Box::new(DrawContext {
             evolution,
             species_index: 0,
-            tab_height: 50.
+            tab_height: 50.,
+            focusing: None,
+            info: None,
+            save_name: None
         }))),
     ).unwrap();
 }
@@ -29,67 +40,115 @@ impl eframe::App for DrawContext {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             let painter = ui.painter();
-
+            let viewport_rect = ctx.input(|i: &egui::InputState| i.screen_rect());
+            let (w, h) = (viewport_rect.width(), viewport_rect.height() - self.tab_height);
+            let mut reset = false;
             draw_top_tab(self, ctx, _frame, painter);
             draw_bottom_tab(self, ctx, _frame, painter);
 
+            ctx.input(|i| {
+                if !i.raw.events.is_empty() {
+                    for event in &i.raw.events {
+                        if let egui::Event::Text(text) = event {
+                            if let Some(name) = &mut self.save_name {
+                                name.push_str(text);
+                            }
+                        }
+                    }
+                }
+            });
+
             if ctx.input(|i| i.key_pressed(Key::Space)) {
                 self.evolution.generation();
-            }
-            if ctx.input(|i| i.key_pressed(Key::ArrowLeft)) {
+                reset = true;
+            }else if ctx.input(|i| i.key_pressed(Key::ArrowLeft)) {
                 self.species_index = self.species_index.checked_sub(1).unwrap_or(0);
+                reset = true;
             }else if ctx.input(|i| i.key_pressed(Key::ArrowRight)) {
                 self.species_index = (self.species_index + 1).min(self.evolution.species().len() - 1);
+                reset = true;
+            }else if ctx.input(|i| i.key_pressed(Key::Escape)) {
+                reset = true;
+            }else if ctx.input(|i| i.key_pressed(Key::S)) {
+                if self.save_name.is_none() && self.focusing.is_some() {
+                    self.save_name = Some(String::new());
+                }
             }
             
-            let tab_height = self.tab_height;
-
-            let viewport_rect = ctx.input(|i: &egui::InputState| i.screen_rect());
-            let (w, h) = (viewport_rect.width(), viewport_rect.height() - tab_height);
-            let padding: f32 = 80.;
-
             let networks = self.evolution.species()[self.species_index].networks();
-            let amount_of_networks = networks.len();
-            let cols = (amount_of_networks as f32).sqrt() as usize; // Number of columns
-            let rows = (amount_of_networks + cols - 1) / cols;    // Number of rows
+            let cols = (networks.len() as f32).sqrt() as usize; // Number of columns
+            let rows = (networks.len() + cols - 1) / cols;    // Number of rows
             
             let cell_width = w / cols as f32;
-            let cell_height = (h - tab_height) / rows as f32;
+            let cell_height = (h - self.tab_height) / rows as f32;
 
             let pos = ctx.input(|i| i.pointer.hover_pos()).unwrap_or(pos2(f32::MIN, f32::MIN));
-            draw_dividers(rows, cols, cell_height, cell_width, w, h, tab_height, painter);
+            if self.focusing.is_none() {
+                draw_dividers(rows, cols, cell_height, cell_width, w, h, self.tab_height, painter);
+            }
+
+            if ctx.input(|i| i.key_pressed(Key::Enter)) {
+                if let Some(save_name) = &self.save_name {
+                    let focus_index = self.focusing.unwrap();
+                    networks[focus_index].save(&save_name);
+                    reset = true;
+                }
+            }
             
+            if reset {
+                self.focusing = None;
+                self.info = None;
+                self.save_name = None;
+            }
+
             let mut coordinates = Vec::new();
             for row in 0..rows {
                 for col in 0..cols {
                     let x = col as f32 * cell_width;
-                    let y = row as f32 * cell_height + tab_height;
+                    let y = row as f32 * cell_height + self.tab_height;
                     coordinates.push((x, y));
                 }
             }
             
-            for (index, network) in networks.iter().enumerate() {
-                let coordinate = coordinates[index];
-                let coordinate_end = (coordinate.0 + cell_width, coordinate.1 + cell_height);
-                let hovering = 
-                    pos.x < coordinate_end.0 && pos.y < coordinate_end.1
-                    && pos.x > coordinate.0 && pos.y > coordinate.1;
-                let clicked = ctx.input(|i| i.pointer.button_released(egui::PointerButton::Primary));
-
-                if hovering && clicked {
-                    println!("==== {:?} =====", self.evolution.species()[self.species_index].get_name());
-                    println!("topology sorted {:?}", network.topological_sort());
-                    println!("{:?}", network);
+            /* Focus and display one network */
+            if let Some(focus_index) = self.focusing {
+                let network = &networks[focus_index];
+                if let Some(info) = draw_network((0., self.tab_height), network, w, h, 60., painter, pos) {
+                    self.info = Some(info);
                 }
+            }
 
-                painter.rect_filled(
-                    Rect {
-                        min: coordinate.into(),
-                        max: pos2(coordinate.0 + cell_width, coordinate.1 + cell_height)
-                    },
-                    0.0, if hovering {Color32::from_rgba_unmultiplied(255, 255, 255, 1)} else {Color32::TRANSPARENT}
-                );
-                draw_network(coordinate, network, cell_width, cell_height, padding, painter);
+            /* Display all nets */
+            else {
+                for (index, network) in networks.iter().enumerate() {
+                    let coordinate = coordinates[index];
+                    let coordinate_end = (coordinate.0 + cell_width, coordinate.1 + cell_height);
+                    let hovering = 
+                        pos.x < coordinate_end.0 && pos.y < coordinate_end.1
+                        && pos.x > coordinate.0 && pos.y > coordinate.1;
+                    let clicked = ctx.input(|i| i.pointer.button_released(egui::PointerButton::Primary));
+                    let double_clicked = ctx.input(|i| i.pointer.button_double_clicked(egui::PointerButton::Primary));
+
+                    if hovering && double_clicked {
+                        self.focusing = Some(index);
+                    }else if hovering && clicked {
+                        println!("==== {:?} =====", self.evolution.species()[self.species_index].get_name());
+                        println!("topology sorted {:?}", network.topological_sort());
+                        println!("{:?}", network);
+                        println!("Node genes: \n{}\n....", network.node_genes().iter().map(|e| e.verbose_debug() + "\n").collect::<String>());
+                    }
+
+                    painter.rect_filled(
+                        Rect {
+                            min: coordinate.into(),
+                            max: pos2(coordinate.0 + cell_width, coordinate.1 + cell_height)
+                        },
+                        0.0, if hovering {Color32::from_rgba_unmultiplied(255, 255, 255, 1)} else {Color32::TRANSPARENT}
+                    );
+                    if let Some(info) = draw_network(coordinate, network, cell_width, cell_height, 60., painter, pos) {
+                        self.info = Some(info);
+                    }
+                }
             }
         });
     }
@@ -101,6 +160,11 @@ fn draw_top_tab(draw_ctx: &mut DrawContext, ctx: &egui::Context, _frame: &mut ef
     let tab_h = draw_ctx.tab_height;
     let viewport_rect = ctx.input(|i: &egui::InputState| i.screen_rect());
     let (w, _) = (viewport_rect.width(), viewport_rect.height());
+
+    painter.line_segment(
+        [(0., draw_ctx.tab_height).into(), (w, draw_ctx.tab_height).into()],
+        (0.2, Color32::GRAY),
+    );
 
     let (mut min_fitness, mut max_fitness) = (f32::MAX, f32::MIN);
     let average = draw_ctx.evolution
@@ -164,9 +228,12 @@ fn draw_top_tab(draw_ctx: &mut DrawContext, ctx: &egui::Context, _frame: &mut ef
             Color32::WHITE
         );
 
+        let line_stroke = if draw_ctx.species_index == index
+                            || draw_ctx.species_index == index.checked_sub(1).unwrap_or(0)
+                            { (1., Color32::GRAY) } else { (0.2, Color32::GRAY) };
         painter.line_segment(
             [(x, 0.).into(), (x, 50.).into()],
-            (0.2, Color32::GRAY),
+            line_stroke
         );
     }
 }
@@ -177,12 +244,33 @@ fn draw_bottom_tab(draw_ctx: &mut DrawContext, ctx: &egui::Context, _frame: &mut
     let tab_h = draw_ctx.tab_height;
     let species = &draw_ctx.evolution.species()[draw_ctx.species_index];
 
-    painter.text(
-        pos2(tab_h / 2., h - tab_h/2.), Align2::LEFT_CENTER,
-        format!("{:<30} | Fit: {:.3}", species.get_name(), species.previous_average_score()),
-        FontId::new(20., egui::FontFamily::Monospace),
-        Color32::WHITE
-    );
+    if let Some(name) = &draw_ctx.save_name {
+        painter.text(
+            pos2(tab_h / 2., h - tab_h/2.), Align2::LEFT_CENTER,
+            format!("Saving as {name:?}"),
+            FontId::new(20., egui::FontFamily::Monospace),
+            Color32::WHITE
+        );
+    }else if let Some(info) = &draw_ctx.info {
+        let type_name = match info.node_type() {
+            NodeGeneType::Input => "Input",
+            NodeGeneType::Output => "Output",
+            NodeGeneType::Regular => "Regular",
+        };
+        painter.text(
+            pos2(tab_h / 2., h - tab_h/2.), Align2::LEFT_CENTER,
+            format!("::{} w_idx->self{:?} id({}) bias({})", type_name, info.incoming_connection_indexes(), info.id(), info.bias()),
+            FontId::new(20., egui::FontFamily::Monospace),
+            Color32::WHITE
+        );
+    }else {
+        painter.text(
+            pos2(tab_h / 2., h - tab_h/2.), Align2::LEFT_CENTER,
+            format!("{:<30} | Fit: {:.3}", species.get_name(), species.previous_average_score()),
+            FontId::new(20., egui::FontFamily::Monospace),
+            Color32::WHITE
+        );
+    }
 }
 
 fn draw_arrow(painter: &Painter, p1: (f32, f32), p2: (f32, f32), path_stroke: impl Into<PathStroke> + Copy) -> () {
@@ -233,7 +321,7 @@ fn rotate_vector(dx: f32, dy: f32, angle: f32) -> (f32, f32) {
 }
 
 fn draw_dividers(rows: usize, cols: usize, cell_height: f32, cell_width: f32, w: f32, h: f32, tab_height: f32, painter: &Painter) -> () {
-    for col in 0..cols + 2 {
+    for col in 1..cols + 2 {
         let y = cell_height * col as f32 + tab_height;
 
         painter.line_segment(
@@ -251,7 +339,15 @@ fn draw_dividers(rows: usize, cols: usize, cell_height: f32, cell_width: f32, w:
     }
 }
 
-fn draw_network(coordinate: (f32, f32), network: &NeatNetwork, cell_width: f32, cell_height: f32, padding: f32, painter: &Painter) -> () {
+/// Returns a node gene if hovering above it
+fn draw_network(
+    coordinate: (f32, f32),
+    network: &NeatNetwork,
+    cell_width: f32, cell_height: f32,
+    padding: f32, painter: &Painter,
+    mouse: Pos2
+) -> Option<NodeGene> {
+    let mut return_node = None;
     painter.text(
         pos2(coordinate.0 + cell_width / 2., coordinate.1 + cell_height - 20.),
         Align2::CENTER_CENTER, format!("{:.3}", network.fitness()),
@@ -300,7 +396,20 @@ fn draw_network(coordinate: (f32, f32), network: &NeatNetwork, cell_width: f32, 
                 NodeGeneType::Output => Color32::BLUE,
             };
 
-            painter.circle_filled(egui::Pos2 { x, y }, NODE_SIZE, col);
+            /* Hovering over node */
+            let hitbox = 5.;
+            let hovering = mouse.x > x - hitbox && mouse.x < x + hitbox
+                            && mouse.y > y - hitbox && mouse.y < y + hitbox;
+
+            if hovering {
+                return_node = Some(node.clone());
+            }
+
+            painter.circle_filled(
+                egui::Pos2 { x, y },
+                if hovering { NODE_SIZE * 1.4 } else { NODE_SIZE },
+                col
+            );
             painter.text(
                 pos2(x + 10., y - 10.),
                 Align2::CENTER_CENTER, format!("{}", node_index),
@@ -326,26 +435,6 @@ fn draw_network(coordinate: (f32, f32), network: &NeatNetwork, cell_width: f32, 
             path_stroke
         );
     }
-}
 
-fn search(conns: &[ConnectionGene], nodes: &[NodeGene], nodes_until_output: usize) -> Option<usize> {
-    for node in nodes {
-        if let NodeGeneType::Output = node.node_type() {
-            return Some(nodes_until_output);
-        }
-        let next_nodes = get_next_nodes(conns, nodes, node);
-        let next_nodes_slice: Vec<NodeGene> = next_nodes.into_iter().cloned().collect();
-        if let Some(result) = search(conns, &next_nodes_slice, nodes_until_output + 1) {
-            return Some(result);
-        }
-    }
-    None
-}
-
-fn get_next_nodes<'a>(conns: &[ConnectionGene], vec: &'a [NodeGene], from: &NodeGene) -> Vec<&'a NodeGene> {
-    vec.iter()
-        .filter(|node| node.incoming_connection_indexes()
-            .iter()
-            .any(|&conn_idx| conns[conn_idx].node_in() == from.id()))
-        .collect()
+    return_node
 }
