363b5dbd12ce2224a688bba091fb573790055ad5
Arturr-H <artur.h.0427@gmail.com>
2024-09-22 17:15:14 +0200

    Implement some useful time related utility functions

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use std::time::{Instant, SystemTime, UNIX_EPOCH};
 
 /// Returns the index of the maximum value in the input vector
 pub fn find_max_index(input: &[f32]) -> usize {
@@ -19,4 +20,32 @@ pub fn get_max_index_list(input: &[f32]) -> Vec<usize> {
     b.sort_by(|&i, &j| input[j].partial_cmp(&input[i]).unwrap());
 
     b
+}
+
+pub struct Timer {
+    name: String,
+    start: Instant,
+}
+
+impl Timer {
+    pub fn start(name: &str) -> Self {
+        let start = Instant::now();
+        println!("Starting {}...", name);
+        Self {
+            name: name.to_string(),
+            start,
+        }
+    }
+
+    pub fn end(self) {
+        let duration = self.start.elapsed();
+        println!("{} took: {:?}", self.name, duration);
+    }
+}
+
+pub fn get_unix_time() -> u128 {
+    let start = SystemTime::now();
+    let since_the_epoch = start.duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    since_the_epoch.as_millis()
 }
