3b2885964a757061735cfdddb2d2db80b5b8d611
Arturr-H <artur.h.0427@gmail.com>
2024-09-24 19:55:04 +0200

    Implement `FitnessEvaluator` trait

---
+++ src/trainer/fitness.rs
@@ -0,0 +1,10 @@
+/* Imports */
+use crate::neural_network::network::NeatNetwork;
+
+/// Any struct passed as a fitness evaluator into the
+/// `Evolution` struct needs to implement this trait.
+pub trait FitnessEvaluator: Clone {
+    /// This function will evaluate a single network, and return
+    /// a fitness score which needs to be >= 0.0.
+    fn run(&mut self, network: &mut NeatNetwork) -> f32;
+}
--- src/trainer/mod.rs
+++ src/trainer/mod.rs
@@ -1,3 +1,4 @@
 pub mod evolution;
 pub mod species;
 pub mod config;
+pub mod fitness;
