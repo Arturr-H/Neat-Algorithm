9621521437e993da5b63c555730c4e4d43b5d380
Arturr-H <artur.h.0427@gmail.com>
2024-09-28 11:41:38 +0200

    Update network tests to account for the new bias node

--- tests/neural_network/network.rs
+++ tests/neural_network/network.rs
@@ -9,13 +9,13 @@ fn initialize_default() -> () {
     assert!(net.output_size() == 2);
     assert!(net.is_input(0));
     assert!(net.is_output(1) && net.is_output(2));
-    assert!(net.get_genes().len() == 2);
+    assert!(net.get_genes().len() == 4);
 }
 
 #[test]
 fn initialize_with_genes() -> () {
     let activations = NetworkActivations::new(Activation::LeakyRelu, Activation::LeakyRelu);
-    let genes = vec![ConnectionGene::new(0, 1, 1., 0)];
+    let genes = vec![ConnectionGene::new(0, 2, 1., 0)];
     let net = NeatNetwork::new_with_genes(1, 2, Arc::default(), Arc::default(), activations, genes, Arc::default());
     assert!(net.input_size() == 1);
     assert!(net.output_size() == 2);
@@ -27,17 +27,13 @@ fn initialize_with_genes() -> () {
 #[test]
 fn calculate_output() -> () {
     let activations = NetworkActivations::new(Activation::LeakyRelu, Activation::LeakyRelu);
-    let genes = vec![ConnectionGene::new(0, 1, 1., 0), ConnectionGene::new(0, 2, 1., 1)];
+    let genes = vec![
+        ConnectionGene::new(0, 2, 1., 0), ConnectionGene::new(0, 1, 1., 1),
+        /* Bias connections below */
+        ConnectionGene::new(3, 2, 1., 0), ConnectionGene::new(3, 1, 1., 1),
+    ];
     let mut net = NeatNetwork::new_with_genes(1, 2, Arc::default(), Arc::default(), activations, genes, Arc::default());
-    assert!(net.calculate_output_cached_topology(vec![1.], false) == vec![1.1, 1.1]);
-}
-
-#[test]
-fn topology_sort() -> () {
-    let activations = NetworkActivations::new(Activation::LeakyRelu, Activation::LeakyRelu);
-    let genes = vec![ConnectionGene::new(0, 1, 1., 0), ConnectionGene::new(0, 2, 1., 1)];
-    let net = NeatNetwork::new_with_genes(1, 2, Arc::default(), Arc::default(), activations, genes, Arc::default());
-    assert!(net.topological_sort() == Some(vec![0, 2, 1]));
+    assert!(net.calculate_output(vec![1.]) == vec![2.1, 2.1]);
 }
 
 #[test]
