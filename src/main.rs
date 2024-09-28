fa46289391ae01242eaff434d9979c73a3446bbc
Arturr-H <artur.h.0427@gmail.com>
2024-09-28 11:28:40 +0200

    Implement snake game evolution

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
-#![allow(dead_code, unused_variables, unused_mut, unused_imports)]
-
 /* Imports */
-use std::{collections::HashMap, os::unix::net, sync::{Arc, Mutex}, time::Duration};
-use neat_algorithm::{debug::display::start_debug_display, games::snake::SnakeGameEvaluator, neural_network::activation::Activation, trainer::{config::mutation::{GenomeMutationProbablities, WeightChangeProbablities}, evolution::Evolution}};
-use rayon::{iter::ParallelIterator, slice::ParallelSliceMut};
-use rand::{thread_rng, Rng};
+use neat_algorithm::{
+    debug::display::start_debug_display,
+    games::snake::SnakeGameEvaluator,
+    neural_network::activation::Activation,
+    trainer::{
+        config::mutation::{
+            GenomeMutationProbablities,
+            WeightChangeProbablities
+        },
+        evolution::Evolution
+    }
+};
 
 fn main() -> () {
-    let mut evolution = Evolution::new()
+    let evolution = Evolution::new()
         .batch_size(25)
         .with_species_size(6)
         .with_input_nodes(8)
-        .with_output_nodes(1)
+        .with_output_nodes(4)
         .mutation_probabilities(GenomeMutationProbablities {
             split_connection: 2,
             create_connection: 5,
-            change_weight: 350,
+            change_weight: 850,
             toggle_weight: 1,
             nothing: 20,
         })
@@ -26,12 +32,14 @@ fn main() -> () {
             multiplication_large: 5,
             change_sign: 3
         })
-        .replace_worst_every_nth_gen(Some(1000))
+        .replace_worst_every_nth_gen(Some(100))
         .with_hidden_activation(Activation::LeakyRelu)
-        .with_output_activation(Activation::LeakyRelu)
+        .with_output_activation(Activation::Sigmoid)
+        .preestablish_connections(true)
         .set_fitness_evaluator(SnakeGameEvaluator)
         .build();
-        
-    
+
+    // let mut net = NeatNetwork::retrieve("/Users/artur/Desktop/snakeman").unwrap();
+    // SnakeGame::score_game(&mut net, 1200, true);
     start_debug_display(evolution);
 }
