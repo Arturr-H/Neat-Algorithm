0d44e3a4683472b5ae43038bae428d694741641f
Arturr-H <artur.h.0427@gmail.com>
2024-10-01 10:00:51 +0200

    Implement more averaging functions

--- src/neural_network/average.rs
+++ src/neural_network/average.rs
@@ -1,3 +1,27 @@
+#[derive(Clone, Copy, Debug)]
+pub enum FitnessAverage {
+    /// α = 0.75
+    Exponential75,
+    /// α = 0.5
+    Exponential50,
+    /// α = 0.25
+    Exponential25,
+
+    /// sum / count
+    Regular,
+}
+
+impl FitnessAverage {
+    pub fn run(&self, values: &[f32]) -> f32 {
+        match self {
+            Self::Exponential25 => exponential_average(values, 0.25),
+            Self::Exponential50 => exponential_average(values, 0.50),
+            Self::Exponential75 => exponential_average(values, 0.75),
+            Self::Regular => values.iter().sum::<f32>() / values.len() as f32,
+        }
+    }
+}
+
 pub fn exponential_average(values: &[f32], alpha: f32) -> f32 {
     let mut avg = 0.0;
     let mut weight_sum = 0.0;
